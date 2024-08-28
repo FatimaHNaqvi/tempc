cad7bad73cec20e630ae6d5c097dfeb12cdcf9a6
Fatima <hnaqvifatima@gmail.com>
2024-08-28 12:12:25 +0400

    Updated according to Nadia's comments

--- src/main/java/io/codejournal/maven/wsdl2java/Conversion.java
+++ src/main/java/io/codejournal/maven/wsdl2java/Conversion.java
@@ -2,34 +2,40 @@ package io.codejournal.maven.wsdl2java;
 
 import java.util.Scanner;
 
-public class Coversion {
+public class Conversion {
     public static void main(String[] args) {
         int choice;
-       do{
-           Scanner in = new Scanner(System.in);
-        System.out.print("Choose:  \n1:CelsiusToFahrenhiet \n2:FahrenhietToCelsius \n3:Exit \n");
-         choice = Integer.parseInt(in.nextLine());
-         float value;
-        switch (choice) {
-            case 1:
-                System.out.print("Enter Number:");
-                value = Float.parseFloat(in.nextLine());
-                TempConversion.CelsiusToFahrenhiet(value);
-                break;
-            case 2:
-                System.out.print("Enter Number:");
-                value = Float.parseFloat(in.nextLine());
-                TempConversion.FahrenhietToCelsius(value);
-                break;
-            case 3:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Invalid Choice");
-                break;
-        }
+        //FIXME Nadia: indentations/formatting (ctrl+a then ctrl+alt+l)
+        // Done Fatima: Done
+        //TODO Nadia: purpose of do-while?
+        // Done Fatima: I want the user to be able to convert temperature as many times as they want. Once done, they can exit
+        do {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Choose:  \n1:CelsiusToFahrenhiet \n2:FahrenhietToCelsius \n3:Exit \n");
+            choice = Integer.parseInt(in.nextLine());
+            System.out.print("Enter Number:");
+            float value;
+            value = Float.parseFloat(in.nextLine());
 
-    }while(choice != 4);
+            switch (choice) {
+                case 1:
+                    //FIXME Nadia: repetitive code. Rather move this outside the switch for both case 1 and 2
+                    //Fixed Fatima-Done
+                    TempConversion.CelsiusToFahrenhiet(value);
+                    break;
+                case 2:
+
+                    TempConversion.FahrenhietToCelsius(value);
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid Choice");
+                    break;
+            }
+
+        } while (choice != 4);
 
     }
 }

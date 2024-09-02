285f6d9bc07cb25a6056663f38d006c82109da1c
Fatima <hnaqvifatima@gmail.com>
2024-09-02 15:46:02 +0400

    Rolled back to origin Conversion file. Rest is same

--- src/main/java/io/codejournal/maven/wsdl2java/Conversion.java
+++ src/main/java/io/codejournal/maven/wsdl2java/Conversion.java
@@ -5,43 +5,31 @@ import java.util.Scanner;
 public class Conversion {
     public static void main(String[] args) {
         int choice;
-        //FIXME Nadia: indentations/formatting (ctrl+a then ctrl+alt+l)
-        // Done Fatima: Done
-        //TODO Nadia: purpose of do-while?
-        // Done Fatima: I want the user to be able to convert temperature as many times as they want. Once done, they can exit
-        do {
-            Scanner in = new Scanner(System.in);
-            System.out.print("Choose:  \n1:CelsiusToFahrenheit \n2:FahrenheitToCelsius \n3:Exit \n");
-            choice = Integer.parseInt(in.nextLine());
-            String Unit;
-            switch (choice) {
-                case 1:
-                    //FIXME Nadia: repetitive code. Rather move this outside the switch for both case 1 and 2
-                    //Fixed Fatima-Done
-                    Unit= "Celsius";
-                    Temp_handler.CelsiusToFahrenheit(Input_temp(Unit));
-                    break;
-                case 2:
-                    Unit= "Fahrenheit";
-                    Temp_handler.FahrenheitToCelsius(Input_temp(Unit));
-                    break;
-                case 3:
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Invalid Choice");
-                    break;
-            }
+       do{
+           Scanner in = new Scanner(System.in);
+        System.out.print("Choose:  \n1:CelsiusToFahrenhiet \n2:FahrenhietToCelsius \n3:Exit \n");
+         choice = Integer.parseInt(in.nextLine());
+         float value;
+        switch (choice) {
+            case 1:
+                System.out.print("Enter Number:");
+                value = Float.parseFloat(in.nextLine());
+                Temp_handler.CelsiusToFahrenheit(value);
+                break;
+            case 2:
+                System.out.print("Enter Number:");
+                value = Float.parseFloat(in.nextLine());
+                Temp_handler.FahrenheitToCelsius(value);
+                break;
+            case 3:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid Choice");
+                break;
+        }
 
-        } while (choice != 4);
+    }while(choice != 4);
 
-
-    }
-        public static float Input_temp(String Unit){
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter Temperature in "+Unit+":");
-        float temp;
-        temp = Float.parseFloat(in.nextLine());
-        return temp;
     }
 }
